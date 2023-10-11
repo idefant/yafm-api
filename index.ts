69a50c464acf62cbf695aca5898b9e88a6e1a444
idefant <inik3004@gmail.com>
2023-10-11 12:32:53 +0500

    fix error "request entity too large"

--- index.ts
+++ index.ts
@@ -11,6 +11,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
+
 app.use(cors());
 app.use(express.json());
 
